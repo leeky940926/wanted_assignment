e0e996b116846a85d0641543f725dfbedfdc29dc
kylee <leeky940926@gmail.com>
2021-10-24 16:13:22 +0900

    [ADD] 포스팅 조회, 페이징기법을 곁들인

--- postings/views.py
+++ postings/views.py
@@ -33,3 +33,29 @@ class PostingView(View) :
 
         except KeyError : 
             return JsonResponse({'message': 'KeyError'}, status=400)
+
+    def get(self, request) :
+        try :
+            offset = int(request.GET.get('offset', 0))
+            limit  = int(request.GET.get('limit', 10))
+
+            if limit > 15 :
+                return JsonResponse({'message' : 'too much list'}, status=400)
+            
+            postings = Posting.objects.all()[offset:offset+limit]
+
+            posting_list = [
+                {
+                    'posting_id' : posting.id,
+                    'title'      : posting.title,
+                    'content'    : posting.content 
+                }
+            for posting in postings]
+
+            return JsonResponse({'message' : posting_list}, status=200)
+
+        except AttributeError :
+            return JsonResponse({'message': 'AttributeError'}, status=400)
+        
+        except TypeError :
+            return JsonResponse({'message': 'TypeError'}, status=400)
